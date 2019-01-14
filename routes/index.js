1344c0c9abd0327b8fb6113fc7347d935b3ba3a3
Pavan Tummala <pavan.tummala@gmail.com>
2019-01-14 11:57:29 -0700

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+const nlu = require('../lib/nlu');
+
+const features = {
+  keywords: {},
+  concepts: {},
+  sentiment: {},
+  entities: {
+  },
+  emotion: {}
+}
+
+router.get('/test', async function(req, res, next) {
+  let response = await nlu.analyze(req.query.text, features);
+  res.json(response);
+});
+
+router.post('/fileUpload', async function(req, res, next) {
+  let response = await nlu.analyze(req.files.file.data.toString('utf8'), features);
+  res.json(response);
+});
+
+module.exports = router;
